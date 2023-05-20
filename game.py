b3029d67e9b0db856af345c03e218bc74a9934a2
Krokin-Alex <alexkaidalov9@gmail.com>
2023-05-19 21:28:45 -0400

    first working release

--- game.py
+++ game.py
@@ -1,16 +1,43 @@
-from utilities import create_world, init_world, print_console_world
+from utilities import (
+    create_world,
+    init_world,
+    print_console_world,
+    print_console_world_debug,
+    clear_screen,
+    build_new_world,
+    count_all_cells,
+)
+import time
+import keyboard
 
 rows = 25
-cols = 100
-world1 = []
-world2 = []
+cols = 80
+world = []
 
 
 def main():
-    world1 = create_world(rows, cols)
-    init_world(world1)
+    generation = 0
 
-    print_console_world(world1)
+    world = create_world(rows, cols)
+    init_world(world)
+    clear_screen()
+    print_console_world(world)
+
+    while True:
+        # Check for Ctrl-C or escape button press
+        if keyboard.is_pressed("Ctrl+C") or keyboard.is_pressed("esc"):
+            break
+
+        generation += 1
+        world = build_new_world(world, rows, cols)
+
+        clear_screen()
+        print_console_world(world)
+        print(
+            "Generaion:", generation, "Number or alive cells:", count_all_cells(world)
+        )
+
+        time.sleep(0.1)
 
 
 if __name__ == "__main__":
--- utilities.py
+++ utilities.py
@@ -1,13 +1,16 @@
+import os
 import random
 
 
-def print_hello_world():
-    print("Hello, world!")
+# Clear console screen
+def clear_screen():
+    # Clear the console screen
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 # Create an empty 2D array filled with None [ [], [], [] ]
 def create_world(rows, columns):
-    worldarray = [[None] * columns for _ in range(rows)]
+    worldarray = [[0] * columns for _ in range(rows)]
     return worldarray
 
 
@@ -23,9 +26,64 @@ def print_console_world(array):
     character = None
     for row in range(len(array)):
         for column in range(len(array[row])):
-            if array[row][column] == 0:
-                character = " "
-            else:
+            if array[row][column] == 1:
                 character = "*"
-            print(character, sep="", end="")
+            else:
+                character = " "
+            print(character, end="")
         print()
+
+
+# Print debug to text console world array
+def print_console_world_debug(array):
+    for row in range(len(array)):
+        for column in range(len(array[row])):
+            print(array[row][column], "", end="")
+        print()
+
+
+# Count how many "alive" cells we have
+def count_all_cells(array):
+    count = 0
+    for row in range(len(array)):
+        for column in range(len(array[row])):
+            count = count + array[row][column]
+    return count
+
+
+# Count neightbours for 3x3 cell
+def count_neighbours(array, row, column):
+    count = 0
+    for i in range(row - 1, row + 2):
+        for j in range(column - 1, column + 2):
+            count = count + array[i][j]
+
+    count = count - array[row][column]
+    return count
+
+
+# Create next generation world, main game logic
+def build_new_world(array, rows, cols):
+    new_world = create_world(rows, cols)
+    count = 0
+
+    for row in range(1, len(array) - 1):
+        for column in range(1, len(array[row]) - 1):
+            # считаем соседей
+            count = count_neighbours(array, row, column)
+
+            # world logic
+            # if cell is alive
+            if array[row][column] == 1:
+                if count < 2 or count > 3:
+                    new_world[row][column] = 0
+                else:
+                    new_world[row][column] = 1
+            # if cell is dead
+            else:
+                if count == 3:
+                    new_world[row][column] = 1
+                else:
+                    new_world[row][column] = 0
+
+    return new_world
